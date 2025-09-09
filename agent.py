f300bb04203f3bf25b49933188181b78ba173e39
rohit20001221 <rohit20001221@gmail.com>
2025-09-10 01:40:47 +0530

    cyber kb

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ from langgraph.prebuilt import create_react_agent
 from langchain_ollama import ChatOllama
 from langgraph.checkpoint.memory import InMemorySaver
 from lib.mitm.startup import start_mitm_proxy
+from lib.tools.kb import search_cyber_kb
 import threading
 
 proxy = threading.Thread(target=start_mitm_proxy)
@@ -16,7 +17,9 @@ llm = ChatOllama(
 print("[x] creating an agent")
 agent = create_react_agent(
     model=llm,
-    tools=[],
+    tools=[
+        search_cyber_kb
+    ],
     checkpointer=InMemorySaver()
 )
 
---
+++ lib/tools/kb.py
@@ -0,0 +1,32 @@
+from langchain_core.tools import tool
+from langchain_ollama.embeddings import OllamaEmbeddings
+from langchain_chroma import Chroma
+import chromadb
+
+embedding_model = OllamaEmbeddings(
+    model="llama3.2"
+)
+
+chroma_client = chromadb.HttpClient(
+    host="127.0.0.1",
+    port=8888,
+    ssl=False
+)
+vector_store = Chroma(
+    client=chroma_client,
+    collection_name="kb_cyber_security",
+    embedding_function=embedding_model
+)
+
+@tool(description="search for the knowledge base for any existing tools that can be used from pen testing")
+def search_cyber_kb(q: str):
+    results = vector_store.similarity_search(
+        q,
+        k=2,
+    )
+
+    response = []
+    for result in results:
+        response.append(result.metadata)
+
+    return response
